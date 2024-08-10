e5f342df39e6640fabb9f69bb74c4f30e94eb49c
GiovannaMoyses <giovanna.moyses20@gmail.com>
2024-08-10 09:10:06 -0300

    commit

---
+++ Aula 27/index.php
@@ -0,0 +1,28 @@
+<?php 
+//adição
+$r = 2+2;
+echo $r;
+$s = "2"+"2";
+echo "<br> $s";
+var_dump($s);
+
+//subtração
+$a = 8-4;
+echo "<br> $a";
+
+//multiplicação 
+$b = 2*2;
+echo "<br> $b";
+
+//divisão
+$c = 8/2;
+echo "<br> $c";
+
+//resto 
+$d = 64%12;
+echo "<br> $d";
+
+//potencia
+$e = 2**2;
+echo "<br> $e";
+?>
